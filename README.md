00a48bc98a3e3109351fb886b25a71ffa81f8c27
aochwang <aochwang@gmail.com>
2019-05-28 22:40:41 +0800

    next.js 搭建 koa初始化

--- README.md
+++
@@ -1,2 +0,0 @@
-# next-koa-demo
-简易搭建next.js koa2 react项目demo
---
+++ server.js
@@ -0,0 +1,31 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev })
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    const server = new Koa();
+    const router = new Router();
+
+    server.use(async (ctx, next) => {
+        await next();
+    });
+
+    server.use(router.routes());
+
+    server.use(async (ctx, next) => {
+        ctx.body = '<span>koa2</span>'
+    })
+
+    server.use(async (ctx, next) => {
+        await handle(ctx.req, ctx.res);
+        ctx.respond = false;
+    });
+
+    server.listen(3000, () => {
+        console.log('测试');
+    });
+})
