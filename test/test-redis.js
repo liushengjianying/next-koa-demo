a1af3f26a75e595820ae81e77a141be7a88619a8
aochwang <aochwang@gmail.com>
2019-05-29 22:04:09 +0800

    ioredis连接node

---
+++ test/test-redis.js
@@ -0,0 +1,17 @@
+
+async function test() {
+    const Redis = require('ioredis');
+
+    const redis = new Redis({
+        port: 6378,
+        password: 123456
+    })
+
+
+    // 数据库是异步的
+    await redis.setex('c', 10 ,123)
+    const keys = await redis.keys('*')
+    console.log(await redis.get('a'), keys)
+}
+
+test()
